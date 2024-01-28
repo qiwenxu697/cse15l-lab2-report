6dfebbb807014f39eae97f55d4ae47125bec6b83
qiwenxu697 <147675962+qiwenxu697@users.noreply.github.com>
2024-01-28 13:53:27 -0800

    Update ChatServer.java

--- ChatServer.java
+++ ChatServer.java
@@ -1,40 +1,54 @@
 import java.io.IOException;
 import java.net.URI;
+import java.util.ArrayList;
 
 class Handler implements URLHandler {
-    // The one bit of state on the server: a number that will be manipulated by
-    // various requests.
-    ArrayList<String> words =new ArrayList<String>();
+    ArrayList<String> messages = new ArrayList<String>();
+    ArrayList<String> users = new ArrayList<String>();
 
     public String handleRequest(URI url) {
         if (url.getPath().equals("/")) {
-            return words.toString();
-        
-        } 
-        else if(url.getPath().contains("/add")){
-            String[] parameters = url.getQuery().split("=");
-            if (parameters[0].equals("s")) {
-                words.add(parameters[1]);
-                return ("Add "+parameters[1]);
-            
+            if (!messages.isEmpty()) {
+                return toString(users,messages);
+            } else {
+                return "No messages yet!";
             }
+        } else if (url.getPath().contains("/add-message")) {
+            String query = url.getQuery();
+            String[] parameters = query.split("&");
+            String message = "";
+            String user = "";
+
+            for (String parameter : parameters) {
+                String[] keyValue = parameter.split("=");
+                if (keyValue.length == 2) {
+                    if (keyValue[0].equals("s")) {
+                        message = keyValue[1].replace("+", " "); // Decode '+' to space for the message
+                        messages.add(message);
+                    } else if (keyValue[0].equals("user")) {
+                        user = keyValue[1];
+                        users.add(user);
+                    }
+                }
+            }
+            return toString(users,messages);
+        } else {
             return "404 Not Found!";
         }
-        else if(url.getPath().contains("/search")){
-            String[] parameters = url.getQuery().split("=");
-            if (parameters[0].equals("s")) {
-                words.add(parameters[1]);
-                return ("Search "+parameters[1]);
-            
-            }
-            return "404 Not Found!";
+    }
+
+    public static String toString(ArrayList<String> l1, ArrayList<String> l2) {
+        String result = "" ;
+        for (int i = 0; i < l1.size(); i++) {
+            result += l1.get(i) + ": " + l2.get(i) + "\n";
         }
+        return result;
     }
 }
 
 class ChatServer {
     public static void main(String[] args) throws IOException {
-        if(args.length == 0){
+        if (args.length == 0) {
             System.out.println("Missing port number! Try any number between 1024 to 49151");
             return;
         }
